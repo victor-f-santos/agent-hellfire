409ac5ae10f6029f1376d9b556367cc83eef1e52
VICTOR SANTOS <victor.f089@gmail.com>
2023-12-06 18:09:20 +0100

    members done

--- db/models/Cart.js
+++ db/models/Cart.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const cartSchema = new Schema({
-  product: { type: String, required: true },
+  product: { type: Schema.Types.ObjectId, ref: "Product" },
   quantity: { type: Number, required: true },
 });
 
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,19 +1,21 @@
 import useSWR from "swr";
 
-export default function Cart() {
-  const { data, error, isLoading } = useSWR("/api/cart");
+export default async function Cart() {
+  //   const { data, error, isLoading } = useSWR("/api/cart");
 
-  if (error) return <p>failed to load</p>;
-  if (isLoading) return <p>loading...</p>;
+  //   if (error) return <p>failed to load</p>;
+  //   if (isLoading) return <p>loading...</p>;
+  //   console.log("data: ", data);
+  //   if (!data || data.length === 0) {
+  //     return <p>No products available</p>;
+  //   }
 
-  if (!data || data.length === 0) {
-    return <p>No products available</p>;
-  }
+  return <h1>hello</h1>;
   return (
     <ul>
-      {data.map((item) => (
+      {/* {data.map((item) => (
         <li key={item._id}>{item.product}</li>
-      ))}
+      ))} */}
     </ul>
   );
 }
