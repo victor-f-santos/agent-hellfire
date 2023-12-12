36aaffd32d2f266c1b0383fd67ce01dab5f860aa
VICTOR SANTOS <victor.f089@gmail.com>
2023-12-12 13:49:26 +0100

    done

--- src/Components/Fotos.js
+++ src/Components/Fotos.js
@@ -1,169 +1,149 @@
 import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Fotos() {
+  const images = [
+    {
+      id: "img1",
+      src: "/img/shooting/IMG_1306.jpg",
+    },
+    {
+      id: "img2",
+      src: "/img/shooting/IMG_1549.jpg",
+    },
+    {
+      id: "img3",
+      src: "/img/shooting/IMG_1619 (1).jpg",
+    },
+    {
+      id: "img4",
+      src: "/img/shooting/IMG_1642.jpg",
+    },
+    {
+      id: "img5",
+      src: "/img/shooting/IMG_6992 Kopie.jpg",
+    },
+    {
+      id: "img6",
+      src: "/img/shooting/IMG_7056 Kopie.jpg",
+    },
+    {
+      id: "img7",
+      src: "/img/shooting/IMG_7110 Kopie.jpg",
+    },
+    {
+      id: "img8",
+      src: "/img/shooting/P8230823 Agent Hellfire _20_0823_c.jpg",
+    },
+    {
+      id: "img9",
+      src: "/img/shooting/P8230947 Agent Hellfire _20_0823_bw.jpg",
+    },
+  ];
+
+  const liveImages = [
+    {
+      id: "live1",
+      src: "/img/live/Live rude.jpg",
+      alt: "Gustavo Mansur and Adans Aldani playing on the stage at Rude Festival",
+    },
+    {
+      id: "live2",
+      src: "/img/live/Live rude_1.jpg",
+      alt: "Victor Munhoz playing on the stage at Rude Festival",
+    },
+    {
+      id: "live3",
+      src: "/img/live/Live rude_2.jpg",
+      alt: "Agent Hellfire playing on the stage at Rude Festival",
+    },
+    {
+      id: "live4",
+      src: "/img/live/Live rude_4.jpg",
+      alt: "Adans Aldani and Victor Munhoz playing on the stage at Rude Festival",
+    },
+    {
+      id: "live5",
+      src: "/img/live/Live rude_5.JPG",
+      alt: "Agent Hellfire and Sepultura on backstage at Rude Festival",
+    },
+    {
+      id: "live6",
+      src: "/img/live/Live sage.jpg",
+      alt: "Agent Hellfire on stage at Sage Club",
+    },
+    {
+      id: "live7",
+      src: "/img/live/Live sage_1.jpg",
+      alt: "Adans Aldani and Gustavo Mansur playing on stage at Sage Club",
+    },
+    {
+      id: "live8",
+      src: "/img/live/Live sage_2.jpg",
+      alt: "Victor Munhoz playing on stage at Sage Club",
+    },
+    {
+      id: "live9",
+      src: "/img/live/Live sage_3.JPG",
+      alt: "Adans Aldani playing on the stage at the Sage Club",
+    },
+    {
+      id: "live10",
+      src: "/img/live/Live stromgitarrenfest.jpg",
+      alt: "Agent Hellfire playing on stage at Stromgitarrenfest",
+    },
+    {
+      id: "live11",
+      src: "/img/live/Live stromgitarrenfest_1.jpg",
+      alt: "Victor Munhoz playing on stage at Stromgitarrenfest",
+    },
+    {
+      id: "live12",
+      src: "/img/live/Live stromgitarrenfest_3.jpg",
+      alt: "Adans Aldani playing on stage at Stromgitarrenfest",
+    },
+    {
+      id: "live13",
+      src: "/img/live/Live stromgitarrenfest_4.jpg",
+      alt: "Adans Aldani and Gustavo Mansur playing on stage at Stromgitarrenfest",
+    },
+  ];
+
   return (
     <>
       <h2 id="fotos">Fotos</h2>
       <h4>Foto Shooting</h4>
-      <section class="fotos--shooting">
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_1306.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_1549.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_1619 (1).jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_1642.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_6992 Kopie.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_7056 Kopie.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/IMG_7110 Kopie.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/P8230823 Agent Hellfire _20_0823_c.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
-        <Image
-          class="shooting-img"
-          src="/img/shooting/P8230947 Agent Hellfire _20_0823_bw.jpg"
-          alt="Agent Hellfire band photo"
-          width={650}
-          height={650}
-        />
+      <section>
+        <Carousel className="carousel--img" infiniteLoop>
+          {images.map((image) => {
+            return (
+              <Image
+                src={image.src}
+                key={image.id}
+                alt="Agent Hellfire band photo"
+                height={650}
+                width={650}
+              />
+            );
+          })}
+        </Carousel>
       </section>
       <h4>Live</h4>
-      <section class="fotos--live">
-        <Image
-          src="/img/live/Live rude.jpg"
-          alt="Gustavo Mansur and Adans Aldani playing on the stage at Rude Festival"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live rude_1.jpg"
-          alt="Victor Munhoz playing on the stage at Rude Festival"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live rude_2.jpg"
-          alt="Agent Hellfire playing on the stage at Rude Festival"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live rude_4.jpg"
-          alt="Adans Aldani and Victor Munhoz playing on the stage at Rude Festival"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live rude_5.JPG"
-          alt="Agent Hellfire and Sepultura on backstage at Rude Festival"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live sage.jpg"
-          alt="Agent Hellfire on stage at Sage Club"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-
-        <Image
-          src="/img/live/Live sage_1.jpg"
-          alt="Adans Aldani and Gustavo Mansur playing on stage at Sage Club"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live sage_2.jpg"
-          alt="Victor Munhoz playing on stage at Sage Club"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live sage_3.JPG"
-          alt="Adans Aldani playing on the stage at the Sage Club"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live stromgitarrenfest.jpg"
-          alt="Agent Hellfire playing on stage at Stromgitarrenfest"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live stromgitarrenfest_1.jpg"
-          alt="Victor Munhoz playing on stage at Stromgitarrenfest"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live stromgitarrenfest_3.jpg"
-          alt="Adans Aldani playing on stage at Stromgitarrenfest"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
-        <Image
-          src="/img/live/Live stromgitarrenfest_4.jpg"
-          alt="Adans Aldani and Gustavo Mansur playing on stage at Stromgitarrenfest"
-          class="shooting-img"
-          width={650}
-          height={650}
-        />
+      <section>
+        <Carousel className="carousel--img" infiniteLoop>
+          {liveImages.map((image) => {
+            return (
+              <Image
+                src={image.src}
+                key={image.id}
+                alt={image.alt}
+                height={650}
+                width={650}
+              />
+            );
+          })}
+        </Carousel>
       </section>
     </>
   );
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,29 +1,19 @@
 import Image from "next/image";
 import Footer from "./Footer";
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Layout({ children }) {
   const [imgSrc, setImgSrc] = useState("/img/Logo_BR_Transparent.png");
   const [imgToggle, setImgToggle] = useState(false);
   const updateSrc = "/img/Logo_BR_Transparent.png";
-  const updateSrc2 = "/img/pentagram.png";
-  const [BackToTop, setBackToTop] = useState(false);
+  const updateSrc2 = "/img/hellfire.gif";
 
   const changeImg = () => {
     setImgSrc(imgToggle ? updateSrc : updateSrc2);
     setImgToggle(!imgToggle);
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        setBackToTop(true);
-      } else {
-        setBackToTop(false);
-      }
-    });
-  }, []);
   return (
     <>
       <main>{children}</main>
@@ -36,17 +26,15 @@ export default function Layout({ children }) {
         />
       </section>
 
-      {BackToTop && (
-        <button className="hellfire">
-          <Image
-            src="/img/pentagram.png"
-            alt="Pentagram"
-            width={50}
-            height={50}
-            onClick={changeImg}
-          />
-        </button>
-      )}
+      <button className="hellfire">
+        <Image
+          src="/img/pentagram.png"
+          alt="Pentagram"
+          width={50}
+          height={50}
+          onClick={changeImg}
+        />
+      </button>
 
       <Footer />
     </>
